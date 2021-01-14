83baf883e6aa6d511e3b1f07d63201bfb6129619
Patrick Pavliuchik <73493957+Pascha-404@users.noreply.github.com>
2021-01-14 19:28:20 +0100

    coded input logic

--- languageTool/index.js
+++ languageTool/index.js
@@ -1,4 +1,53 @@
-const franc = require ('franc');
-const colors = require ('colors');
-const langs = require ('langs');
+const franc = require('franc');
+const colors = require('colors');
+const langs = require('langs');
 
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+function translate(input) {
+    const code = franc(input);
+    if (code === 'und' || code === 'undefined') {
+        console.log('');
+        console.log(colors.red.bgBrightYellow('####################'))
+        console.log(colors.red.bgBrightYellow('####################'))
+        console.log(colors.red.bgBrightYellow('DETECTION FAILED'))
+        console.log(colors.red.bgBrightYellow('TRY DIFFERENT'))
+        console.log(colors.red.bgBrightYellow('OR LONGER'))
+        console.log(colors.red.bgBrightYellow('SENTENCE!'))
+        console.log(colors.red.bgBrightYellow('####################'))
+        console.log(colors.red.bgBrightYellow('####################'))
+        console.log('');
+    } else {
+        const codeTrans = langs.where('3', code);
+        if (codeTrans.name === '') {
+            console.log('');
+            console.log(color.red('Missing Informations In Database!'));
+            console.log('');
+        } else {
+            console.log('');
+            console.log(colors.green('-----------------------------------'));
+            console.log('');
+            console.log(colors.green('The language you are searching is:'));
+            console.log(colors.green(codeTrans.name));
+            console.log('');
+            console.log(colors.green('Localy spoken it is:'));
+            console.log(colors.green(codeTrans.local));
+            console.log('');
+            console.log(colors.green('-----------------------------------'));
+            console.log('');
+        }
+    }
+}
+
+readline.question(colors.bgWhite.black(`Write down what you want to have detected -> `), (input) => {
+    try {
+        translate(input);
+    } catch (e) {
+        console.log(colors.red('Detection ERROR!'));
+        console.log(e)
+    }
+    readline.close();
+})
